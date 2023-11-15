90cdb3bb235ad5d5fca12608398bff336a5350d4
alaska113 <alaska113@gmail.com>
2023-11-14 20:00:11 -0800

    Start education page

--- src/pages/Education.tsx
+++ src/pages/Education.tsx
@@ -1,15 +1,21 @@
 import { Typography } from "@mui/material";
 import Page from "../components/Page";
+import '../styles/Education.css';
 
 const Eductation: React.FC = () => {
     console.log('education');
     return (
         <Page>
-            <div>
-                <Typography>Education</Typography>
-                <Typography>University of Alaska Fairbanks</Typography>
-                <Typography>Physics Stuff</Typography>
-                <Typography>Computer Science stuff</Typography>
+            <div className='education'>
+                <Typography variant="h5">University of Alaska Fairbanks</Typography>
+                <img src='uaf-westridge-aerial.jpg' alt="university-of-alaska-fairbanks-aerial" className='uaf-image'/>
+                <div className="summary-text">
+                    <Typography>B.S. Computational Physics ✅</Typography>
+                    <Typography>Minor in Mathematics</Typography>
+                    <Typography>Data Structures and Algorithms</Typography>
+                    <hr className="summary-divider"/>
+                </div>
+                <Typography>Test</Typography>
             </div>
         </Page>
     );
