ac7425d0578f0a2b0c25f875b2ff64f1a33fb5e0
alaska113 <alaska113@gmail.com>
2023-11-17 13:22:13 -0800

    Fill in home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,18 @@
 import { Typography } from "@mui/material";
 import Page from "../components/Page";
+import { useNavigate } from "react-router-dom";
 
 const Home: React.FC  = () => {
-    console.log('test');
+    const navigate = useNavigate();
     return (
         <Page>
-            <Typography>Home</Typography>
+            <div className='home'>
+                <div>
+                    <Typography variant="h3">Hey there, I'm Khan.</Typography>
+                    <Typography variant="h5">A Full-Stack Engineer.</Typography>
+                    <Typography variant="h5" style={{marginTop: '10px'}}>Check out my <a href=''>resume</a> or learn more about me <span className='about-link' onClick={() => {navigate('/about')}}>here</span>.</Typography>
+                </div>
+            </div>
         </Page>
     );
 }
