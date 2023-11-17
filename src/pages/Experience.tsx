5b3b5932759abea2a134f39b85e483e3681839fc
alaska113 <alaska113@gmail.com>
2023-11-17 15:46:19 -0800

    Clean up experience and delete unessicary company logos

--- src/pages/Experience.tsx
+++ src/pages/Experience.tsx
@@ -5,7 +5,6 @@ interface ExperienceItem {
     title: string;
     company: string;
     dateRange: string;
-    logoSource: string;
     description: string;
     bulletPoints: string[];
 }
@@ -15,7 +14,6 @@ const experienceList: ExperienceItem[] = [
         title: 'Full-Stack Engineer',
         company: 'Class Technologies Inc.',
         dateRange: '2021-2023',
-        logoSource: '/class.jpg',
         description: 'As a Full Stack Engineer at Class Technologies, I excelled in both the creation and maintenance of React-powered admin and in-app pages.',
         bulletPoints: [
             'Designed and implemented TypeScript API endpoints hosted on AWS Lambda via the Serverless framework.',
@@ -30,7 +28,6 @@ const experienceList: ExperienceItem[] = [
         title: 'Full-Stack Engineer',
         company: 'Attently Inc.',
         dateRange: '2018-2020',
-        logoSource: '/attently.jpeg',
         description: 'At Attently, I took a lead role in crafting and implementing sophisticated data analysis tools, which were instrumental in dissecting facial expression data for valuable customer feedback metrics.',
         bulletPoints: [
             'Implemented AWS services like Kinesis, Rekognition, DynamoDB, S3, CloudWatch, and Cognito to efficiently handle and manipulate large volumes of customer data.',
@@ -43,17 +40,16 @@ const experienceList: ExperienceItem[] = [
 
 
 
-const ExperienceSection: React.FC<ExperienceItem> = ({ title, company, dateRange, logoSource, description, bulletPoints }) => {
+const ExperienceSection: React.FC<ExperienceItem> = ({ title, company, dateRange, description, bulletPoints }) => {
     return (
         <div className='experience-section'>
             <div className='experience-header'>
-                <div className='company-logo'>
-                    <img src={logoSource} alt='class-logo' className='company-logo-img'/>
+                <div>
+                    <Typography variant='h5'>{company}</Typography>
+                    <Typography>{dateRange}</Typography>
                 </div>
                 <div className='experience-info'>
-                    <Typography variant='h5'>{title}</Typography>
-                    <Typography variant='h6'>{company}</Typography>
-                    <Typography>{dateRange}</Typography>
+                    <Typography variant='h6'>{title}</Typography>
                 </div>
             </div>
             <hr/>
@@ -79,7 +75,6 @@ const Experience: React.FC = () => {
                 title={item.title}
                 company={item.company}
                 dateRange={item.dateRange}
-                logoSource={item.logoSource}
                 description={item.description}
                 bulletPoints={item.bulletPoints}
             />)}
