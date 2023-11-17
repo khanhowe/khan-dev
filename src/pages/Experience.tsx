a1dfa2fbdfd6a71034bff2047c72f6e370a3b01d
alaska113 <alaska113@gmail.com>
2023-11-16 16:43:49 -0800

    Make experience sections more modular for quicker experience adding

--- src/pages/Experience.tsx
+++ src/pages/Experience.tsx
@@ -1,17 +1,39 @@
 import { Typography } from "@mui/material";
 import '../styles/Experience.css';
 
-const ExperienceSection: React.FC = () => {
+interface ExperienceItem {
+    title: string;
+    company: string;
+    dateRange: string;
+    logoSource: string;
+    description: string[];
+}
+
+const experienceList: ExperienceItem[] = [
+    {
+        title: 'Full-Stack Engineer',
+        company: 'Class Technologies Inc.',
+        dateRange: '2021-2023',
+        logoSource: '/class.jpg',
+        description: [
+            'blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah',
+        ]
+    }
+];
+
+
+
+const ExperienceSection: React.FC<ExperienceItem> = ({ title, company, dateRange, logoSource, description }) => {
     return (
         <div className='experience-div'>
             <div>
-                <img src='/class.jpg' alt='class-logo' className='company-logo'/>
+                <img src={logoSource} alt='class-logo' className='company-logo'/>
             </div>
             <div className='experience-text-div'>
-                <Typography variant='h5'>Full-stack Engineer</Typography>
-                <Typography variant='h6'>Class Technologies Inc.</Typography>
-                <Typography>2021-2023</Typography>
-                <Typography>blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah</Typography>
+                <Typography variant='h5'>{title}</Typography>
+                <Typography variant='h6'>{company}</Typography>
+                <Typography>{dateRange}</Typography>
+                {description.map((paragraph, index) => <Typography key={index}>{paragraph}</Typography>)}
             </div>
         </div>  
     );
@@ -21,9 +43,14 @@ const Experience: React.FC = () => {
     console.log('test');
     return (
         <div className='card-list'>
-            <ExperienceSection/>
-            <hr/>
-            <ExperienceSection/>
+            {experienceList.map((item, index) => <ExperienceSection 
+                key={index} 
+                title={item.title}
+                company={item.company}
+                dateRange={item.dateRange}
+                logoSource={item.logoSource}
+                description={item.description}
+            />)}
         </div>
     );
 }
