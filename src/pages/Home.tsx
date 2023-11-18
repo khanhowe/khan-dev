c789d833defa5e7f18db5cde12bf29dc10f21c73
alaska113 <alaska113@gmail.com>
2023-11-18 13:02:41 -0800

    Change word from here to below for mobile

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,17 +3,19 @@ import Page from "../components/Page";
 import { useNavigate } from "react-router-dom";
 import useFadeInEffect from "../hooks/useFadeInEffect";
 import '../styles/Home.scss';
+import { useIsSmScreen } from "../hooks/useScreenSize";
 
 const Home: React.FC = () => {
     const navigate = useNavigate();
     const isVisible = useFadeInEffect();
+    const isSmallScreen = useIsSmScreen();
     return (
         <Page>
             <div className={`home ${isVisible ? 'fade-in' : 'fade-out'}`}>
                 <div>
                     <Typography variant="h3">Hey there, I'm Khan.</Typography>
                     <Typography variant="h5">A Full-Stack Engineer.</Typography>
-                    <Typography variant="h5" style={{marginTop: '10px'}}>Download my <a href='/Resume110823.pdf' download='khan-howe-resume.pdf'>resume</a> or learn more about me <span className='about-link' onClick={() => {navigate('/about')}}>here</span>.</Typography>
+                    <Typography variant="h5" style={{marginTop: '10px'}}>Download my <a href='/Resume110823.pdf' download='khan-howe-resume.pdf'>resume</a> or learn more about me {isSmallScreen ? 'below' : <span className='about-link' onClick={() => {navigate('/about')}}>here</span>}.</Typography>
                 </div>
             </div>
         </Page>
