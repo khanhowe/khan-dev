6d554eafb2fc29b9cb3bfc5c28ecb3b59dc6e1fc
alaska113 <alaska113@gmail.com>
2024-02-27 17:04:45 -0800

    Setup style for new app bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,26 @@
 import './styles/App.css';
 import { ThemeProvider} from '@mui/material';
 import theme from './theme';
-import { BrowserRouter as Router } from 'react-router-dom';
 import About from './pages/About';
 import Projects from './pages/Projects';
 import Background from './pages/Background';
 import Footer from './components/Footer';
 import Header from './components/Header';
+import NavBar from './components/NavBar';
 
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <div className="App">
+        <NavBar/>
         <div className='view-paper'>
-          <Router>
-            <Header/>
+            {/* <Header/> */}
             <div className='app-body'>
               <About/>
               <Background/>
               <Projects/>
             </div>
-          </Router>
         </div>
         <Footer/>
       </div>
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,51 +1,94 @@
-import { Tab, Tabs } from "@mui/material";
-import React, { useEffect, useMemo, useState } from "react";
-import { useLocation, useNavigate } from 'react-router-dom';
+import { AppBar, Box, Button, Container, IconButton, Menu, MenuItem, Toolbar, Typography } from '@mui/material';
+import CodeIcon from '@mui/icons-material/Code';
+import MenuIcon from '@mui/icons-material/Menu';
 import '../styles/NavBar.css';
-
-interface TabData {
-    title: string;
-    route: string;
-}
-
-const a11yProps = (index: number) => {
-    return {
-        id: `tab-${index}`,
-        'aria-controls': `tabpanel-${index}`
-    };
-};
+import { useMemo, useState } from 'react';
 
 const NavBar: React.FC = () => {
-    const location = useLocation();
-    const [pageSelect, setPageSelect] = useState<number>(0);
-    const navigate = useNavigate();
+    const [anchorElNav, setAnchorElNav] = useState<null | HTMLElement>(null);
     
-    const tabs: TabData[] = useMemo(() => [
-        { title: 'Home', route: '/'},
-        { title: 'About', route: '/about'},
-        { title: 'Background', route: '/background'},
-        { title: 'Projects', route: '/projects'}
+    const sections: string[] = useMemo(() => [
+        'About',
+        'Skills',
+        'Experience',
+        'Education',
+        'Projects'
     ], []);
 
-    const handleChange = (event: React.SyntheticEvent, newValue: number) => {
-        setPageSelect(newValue);
-        navigate(tabs[newValue].route);
+    const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorElNav(event.currentTarget);
     };
-
-    useEffect(() => {
-        const selectedTabIndex = tabs.findIndex(tab => tab.route === location.pathname);
-        if (selectedTabIndex !== -1) {
-            setPageSelect(selectedTabIndex);
-        }
-    }, [location.pathname, tabs]);
-
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+    };
     return (
-        <div className='navbar-div'>
-            <Tabs value={pageSelect} onChange={handleChange}   textColor="primary" indicatorColor="primary" className='navbar' variant='fullWidth'>
-                {tabs.map((tab, index) => <Tab label={tab.title}  className='navbar-tab' {...a11yProps(index)} key={index}/>)}
-            </Tabs>
-        </div>
+        <AppBar position='fixed'>
+            <Container maxWidth='xl' className='navbar'>
+                <Toolbar sx={{ display: 'flex', alignItems: 'center' }}>
+                    <IconButton
+                        sx={{ display: { sm: 'flex', md: 'none'}, mr: 2 }}
+                        size="large"
+                        edge="start"
+                        color="inherit"
+                        aria-label="open drawer"
+                        onClick={handleOpenNavMenu}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <Menu
+                        id="menu-appbar"
+                        anchorEl={anchorElNav}
+                        anchorOrigin={{
+                            vertical: 'bottom',
+                            horizontal: 'left',
+                        }}
+                        keepMounted
+                        transformOrigin={{
+                            vertical: 'top',
+                            horizontal: 'left',
+                        }}
+                        open={Boolean(anchorElNav)}
+                        onClose={handleCloseNavMenu}
+                        sx={{
+                            display: { xs: 'block', md: 'none' },
+                        }}
+                        >
+                        {sections.map((section) => (
+                            <MenuItem key={section} onClick={handleCloseNavMenu}>
+                                <Typography textAlign="center">{section}</Typography>
+                            </MenuItem>
+                        ))}
+                    </Menu>
+                    <Box>
+                        <div className='logo'>
+                            <CodeIcon 
+                                sx={{ display: { sm: 'none', md: 'flex' }, mr: 1 }}
+                                color="inherit"
+                                fontSize='large'
+                            />
+                            <Typography variant='h5'>Khan Howe</Typography>
+                        </div>
+                    </Box>
+                    <Box sx={{ 
+                        flexGrow: 1,
+                        display: { xs: 'none', sm: 'none', md: 'flex' },
+                        justifyContent: 'flex-end',
+                        alignItems: 'center',
+                    }}>
+                        {sections.map((section) => (
+                        <Button
+                            key={section}
+                            onClick={handleCloseNavMenu}
+                            sx={{ my: 2, color: 'white', display: 'block' }}
+                        >
+                            {section}
+                        </Button>
+                        ))}
+                    </Box>
+                </Toolbar>
+            </Container>
+        </AppBar>
     );
-};
+}
 
 export default NavBar;
