c56af8771554e2e62009000e013eb0b45d809e07
alaska113 <alaska113@gmail.com>
2024-03-07 17:10:27 -0800

    Make experience cards more responsive

--- src/pages/Experience.tsx
+++ src/pages/Experience.tsx
@@ -110,7 +110,8 @@ export const ExperienceSection: React.FC<ExperienceItem> = ({
                     sx={{
                         display: 'flex',
                         justifyContent: 'space-between',
-                        alignItems: 'center',
+                        alignItems: { xs: 'flex-start', sm: 'center' },
+                        flexDirection: { xs: 'column', sm: 'row' },
                     }}
                 >
                     <ResponsiveTypography variant="body1">
